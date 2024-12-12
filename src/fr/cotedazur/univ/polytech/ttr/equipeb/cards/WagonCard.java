a63394a73aff0d3363a3bf883ff3254e6f946ae4
Rayan Outili <rayanout2005@gmail.com>
2024-12-12 20:54:37 +0100

    fix: delete duplicate #5

--- src/fr/cotedazur/univ/polytech/ttr/equipeb/cards/WagonCard.java
+++
@@ -1,6 +0,0 @@
-package fr.cotedazur.univ.polytech.ttr.equipeb.cards;
-
-public class WagonCard extends Card {
-    public WagonCard() {
-    }
-}
--- src/fr/cotedazur/univ/polytech/ttr/equipeb/cards/models/WagonCard.java
+++
@@ -1,6 +0,0 @@
-package fr.cotedazur.univ.polytech.ttr.equipeb.cards.models;
-
-public class WagonCard extends Card {
-    public WagonCard() {
-    }
-}
