724f1f31e0899f9ebff46818b0b667f16eb7cf2e
Matvei <legomegamat@gmail.com>
2024-12-13 10:01:47 +0100

    Fix: make the card deck general to all cards #14

--- test/fr/cotedazur/univ/polytech/ttr/equipeb/models/deck/CardDeckTestWithWagonCards.java
+++ test/fr/cotedazur/univ/polytech/ttr/equipeb/models/deck/CardDeckTestWithWagonCards.java
@@ -1,5 +1,6 @@
 package fr.cotedazur.univ.polytech.ttr.equipeb.models.deck;
 
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.cards.Card;
 import fr.cotedazur.univ.polytech.ttr.equipeb.models.cards.WagonCard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class WagonCardDeckTest {
-    WagonCardDeck wagonCardDeckInitEmpty;
-    WagonCardDeck wagonCardDeckInitFilled;
+class CardDeckTestWithWagonCards {
+    CardDeck wagonCardDeckInitEmpty;
+    CardDeck wagonCardDeckInitFilled;
 
     WagonCard card1;
     WagonCard card2;
@@ -18,12 +19,12 @@ class WagonCardDeckTest {
 
     @BeforeEach
     void setUp() {
-        wagonCardDeckInitEmpty = new WagonCardDeck();
+        wagonCardDeckInitEmpty = new CardDeck();
         card1 = new WagonCard();
         card2 = new WagonCard();
         card3 = new WagonCard();
-        List<WagonCard> cards = List.of(card1, card2, card3);
-        wagonCardDeckInitFilled = new WagonCardDeck(cards);
+        List<Card> cards = List.of(card1, card2, card3);
+        wagonCardDeckInitFilled = new CardDeck(cards);
     }
 
     @Test
@@ -34,11 +35,11 @@ class WagonCardDeckTest {
 
     @Test
     void testPickCard() {
-        WagonCard card = wagonCardDeckInitFilled.drawCard();
+        Card card = wagonCardDeckInitFilled.drawCard();
         assertEquals(2, wagonCardDeckInitFilled.size());
         assertEquals(card, card1);
-        assertEquals(card2, wagonCardDeckInitFilled.getWagonCard(0));
-        assertEquals(card3, wagonCardDeckInitFilled.getWagonCard(1));
+        assertEquals(card2, wagonCardDeckInitFilled.getCard(0));
+        assertEquals(card3, wagonCardDeckInitFilled.getCard(1));
     }
 
     @Test
@@ -49,9 +50,9 @@ class WagonCardDeckTest {
     @Test
     void testRegenerateDeck() {
         WagonCard card4 = new WagonCard();
-        List<WagonCard> cards = List.of(card4);
+        List<Card> cards = List.of(card4);
         wagonCardDeckInitEmpty.regenerateDeck(cards);
         assertEquals(1, wagonCardDeckInitEmpty.size());
-        assertEquals(card4, wagonCardDeckInitEmpty.getWagonCard(0));
+        assertEquals(card4, wagonCardDeckInitEmpty.getCard(0));
     }
 }
