2a665f26f555c5f3c27a9aa22635493e07dbd894
Matvei <legomegamat@gmail.com>
2024-12-13 10:51:07 +0100

    Test: add objective card test, adding to hand and using a deck #14

--- src/fr/cotedazur/univ/polytech/ttr/equipeb/Main.java
+++ src/fr/cotedazur/univ/polytech/ttr/equipeb/Main.java
@@ -3,7 +3,9 @@ package fr.cotedazur.univ.polytech.ttr.equipeb;
 import fr.cotedazur.univ.polytech.ttr.equipeb.exceptions.RouteAlreadyClaimedException;
 import fr.cotedazur.univ.polytech.ttr.equipeb.map.City;
 import fr.cotedazur.univ.polytech.ttr.equipeb.map.Route;
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.cards.ObjectiveCard;
 import fr.cotedazur.univ.polytech.ttr.equipeb.models.cards.WagonCard;
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.deck.CardDeck;
 import fr.cotedazur.univ.polytech.ttr.equipeb.players.Player;
 import fr.cotedazur.univ.polytech.ttr.equipeb.exceptions.NotEnoughCardsException;
 
@@ -22,6 +24,12 @@ public class Main {
         player.pickCard(new WagonCard());
         player.pickCard(new WagonCard());
         player.pickCard(new WagonCard());
+        CardDeck deck = new CardDeck();
+        deck.addCard(new ObjectiveCard());
+        deck.addCard(new ObjectiveCard());
+        deck.addCard(new ObjectiveCard());
+        player.pickCard(deck.drawCard());
+        player.pickCard(deck.drawCard());
         System.out.println("Player's hand : " + player.getHand());
 
         System.out.println("Player tries to claim a route between"
