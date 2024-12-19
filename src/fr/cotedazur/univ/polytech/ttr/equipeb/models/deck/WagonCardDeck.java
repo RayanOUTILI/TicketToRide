73c78ff909839a930210a15058a2df55208c01cd
Rayan Outili <rayanout2005@gmail.com>
2024-12-19 20:42:13 +0100

    fix: delete wrong file

---
+++ src/fr/cotedazur/univ/polytech/ttr/equipeb/models/deck/WagonCardDeck.java
@@ -0,0 +1,56 @@
+package fr.cotedazur.univ.polytech.ttr.equipeb.models.deck;
+
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.cards.WagonCard;
+
+import java.util.*;
+
+/**
+ * Class representing the deck of wagon cards (hidden stack of cards)
+ */
+public class WagonCardDeck {
+    private final Deque<WagonCard> stack;
+    private final Deque<WagonCard> discardPile = new ArrayDeque<>();
+
+
+    public WagonCardDeck(List<WagonCard> cards) {
+        this.stack = new ArrayDeque<>(cards);
+    }
+
+    /**
+     * @return the first card of the deck and remove it from the deck
+     */
+    public WagonCard drawCard() {
+        if (stack.isEmpty()) {
+            throw new IllegalStateException("The deck is empty!");
+        }
+        return stack.removeFirst();
+    }
+
+    public boolean addCardToDiscardPile(List<WagonCard> cards) {
+        return discardPile.addAll(cards);
+    }
+
+    public boolean fillDeck() {
+        if (stack.isEmpty()) {
+            stack.addAll(discardPile);
+            discardPile.clear();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return true if the deck is empty
+     */
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public boolean shuffle() {
+        List<WagonCard> cards = new ArrayList<>(stack);
+        Collections.shuffle(cards);
+        stack.clear();
+        stack.addAll(cards);
+        return true;
+    }
+}
