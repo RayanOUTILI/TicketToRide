b5c387feb2f26e7a9561199f5ebeb75df8bfa229
louis rainero <louis.rainero@etu.unice.fr>
2024-12-19 18:25:29 +0100

    tests: GameModel

---
+++ test/fr/cotedazur/univ/polytech/ttr/equipeb/models/game/GameModelTest.java
@@ -0,0 +1,89 @@
+package fr.cotedazur.univ.polytech.ttr.equipeb.models.game;
+
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.cards.DestinationCard;
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.cards.WagonCard;
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.deck.DestinationCardDeck;
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.deck.WagonCardDeck;
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.map.Route;
+import fr.cotedazur.univ.polytech.ttr.equipeb.players.models.PlayerModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+class GameModelTest {
+    private GameModel gameModel;
+    private List<PlayerModel> players;
+    private WagonCardDeck wagonCardDeck;
+    private DestinationCardDeck destinationCardDeck;
+    private Route route;
+    private List<Route> routes;
+
+
+    @BeforeEach
+    public void setUp() {
+        this.players = List.of(mock(PlayerModel.class));
+        this.wagonCardDeck = mock(WagonCardDeck.class);
+        this.destinationCardDeck = mock(DestinationCardDeck.class);
+        this.route = mock(Route.class);
+        this.routes = List.of(route);
+        gameModel = new GameModel(players, wagonCardDeck, destinationCardDeck, routes);
+    }
+
+    @Test
+    void testIsAllRoutesClaimed() {
+        this.routes.forEach(route -> when(route.isClaimed()).thenReturn(true));
+        assertTrue(gameModel.isAllRoutesClaimed());
+    }
+
+    @Test
+    void testIsWagonCardDeckEmpty() {
+        when(wagonCardDeck.isEmpty()).thenReturn(true);
+        assertTrue(gameModel.isWagonCardDeckEmpty());
+    }
+
+    @Test
+    void testDrawWagonCard() {
+        WagonCard wagonCard = mock(WagonCard.class);
+        when(wagonCardDeck.drawCard()).thenReturn(wagonCard);
+        assertEquals(gameModel.drawCardFromWagonCardDeck(), wagonCard);
+    }
+
+    @Test
+    void testGetNonControllableRoutes() {
+        assertEquals(gameModel.getNonControllableRoutes(), routes);
+    }
+
+    @Test
+    void testGetRoute() {
+        when(route.getId()).thenReturn(1);
+        assertEquals(gameModel.getRoute(1), route);
+    }
+
+    @Test
+    void testIsDestinationCardDeckEmpty() {
+        when(destinationCardDeck.isEmpty()).thenReturn(true);
+        assertTrue(gameModel.isDestinationCardDeckEmpty());
+    }
+
+    @Test
+    void testDrawDestinationCards() {
+        List<DestinationCard> destinationCards = List.of(mock(DestinationCard.class));
+        when(destinationCardDeck.drawCard(1)).thenReturn(destinationCards);
+        when(destinationCardDeck.drawCard(3)).thenReturn(destinationCards);
+        assertEquals(gameModel.drawDestinationCards(1), destinationCards);
+        assertEquals(gameModel.drawDestinationCards(3), destinationCards);
+    }
+
+    @Test
+    void testReturnDestinationCardsToTheBottom() {
+        List<DestinationCard> destinationCards = List.of(mock(DestinationCard.class));
+        gameModel.returnDestinationCardsToTheBottom(destinationCards);
+        verify(destinationCardDeck).addCardsAtBottom(destinationCards);
+    }
+}
