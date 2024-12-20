0c04d84cd7309d6d117a1ea99d8d6dfbc4fbbe34
louis rainero <louis.rainero@etu.unice.fr>
2024-12-20 17:03:51 +0100

    feature: add color on the console

--- src/fr/cotedazur/univ/polytech/ttr/equipeb/players/models/PlayerIdentification.java
+++ src/fr/cotedazur/univ/polytech/ttr/equipeb/players/models/PlayerIdentification.java
@@ -4,9 +4,20 @@ package fr.cotedazur.univ.polytech.ttr.equipeb.players.models;
  * Enumeration to identify the players
  */
 public enum PlayerIdentification {
-    BLUE,
-    GREEN,
-    JAUNE,
-    RED,
-    BLACK
+    BLUE("\u001B[34m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    RED("\u001B[31m"),
+    BLACK("\u001B[30m");
+
+    private final String code;
+
+    PlayerIdentification(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return code + super.toString() + "\u001B[0m";
+    }
 }
