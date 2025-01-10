54b165a2f4f6ca1de674cf070bd69078232e2a21
Maksimenka Matvei <70953270+MatveiMKS@users.noreply.github.com>
2025-01-10 12:13:01 +0100

    fix: rename test class so it is considered when running tests

--- test/fr/cotedazur/univ/polytech/ttr/equipeb/models/deck/CardDeckWithObjectiveCardsTest.java
+++ test/fr/cotedazur/univ/polytech/ttr/equipeb/models/deck/CardDeckWithObjectiveCardsTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class CardDeckTestWithObjectiveCards {
+class CardDeckWithObjectiveCardsTest {
     CardDeck cardDeckInitEmpty;
     CardDeck cardDeckInitFilled;
 
