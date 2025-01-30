d7ade44928fa99784fc6eb23a289b2bec0b982ea
Louis Rainero <louis.rainero@etu.univ-cotedazur.fr>
2025-01-30 20:37:22 +0100

    refactor: replace print by logger

--- src/fr/cotedazur/univ/polytech/ttr/equipeb/views/GameConsoleView.java
+++ src/fr/cotedazur/univ/polytech/ttr/equipeb/views/GameConsoleView.java
@@ -2,15 +2,24 @@ package fr.cotedazur.univ.polytech.ttr.equipeb.views;
 
 import fr.cotedazur.univ.polytech.ttr.equipeb.players.models.PlayerIdentification;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class GameConsoleView implements IGameViewable {
+    private final Logger logger = Logger.getLogger(GameConsoleView.class.getSimpleName());
+
+    public GameConsoleView() {
+        logger.setLevel(Level.INFO);
+    }
+
     @Override
     public void displayEndGameReason(PlayerIdentification playerId, int nbOfWagons) {
-        System.out.println("Player " + playerId + " has run out of wagons");
+        logger.log(Level.INFO, "Player " + playerId + " has run out of wagons");
     }
 
     @Override
     public void displayWinner(PlayerIdentification playerId, int score) {
-        System.out.println("Player " + playerId + " won with a score of " + score);
+        logger.log(Level.INFO, "Player " + playerId + " won with a score of " + score);
     }
 
 }
