c65280fe0dda17f335a054cdd7ce78d687ae7d32
Louis Rainero <louis.rainero@etu.univ-cotedazur.fr>
2025-01-30 13:02:18 +0100

    tests: GameEngine InitGame and InitPlayers

--- test/fr/cotedazur/univ/polytech/ttr/equipeb/engine/GameEngineTest.java
+++ test/fr/cotedazur/univ/polytech/ttr/equipeb/engine/GameEngineTest.java
@@ -7,11 +7,13 @@ import fr.cotedazur.univ.polytech.ttr.equipeb.models.game.GameModel;
 import fr.cotedazur.univ.polytech.ttr.equipeb.players.Player;
 import fr.cotedazur.univ.polytech.ttr.equipeb.views.IGameViewable;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 class GameEngineTest {
 
@@ -35,6 +37,24 @@ class GameEngineTest {
         scoreController = mock(ScoreController.class);
         players = List.of(player1, player2);
         gameEngine = new GameEngine(gameModel, players, gameView, Map.of(Action.PICK_WAGON_CARD, controller), scoreController);
+    }
+
+    @Test
+    void testInitGame() {
+        when(controller.initGame()).thenReturn(true);
+
+        assertTrue(gameEngine.initGame());
+        verify(controller, times(1)).initGame();
+    }
+
+    @Test
+    void testInitPlayer() {
+        when(controller.initPlayer(player1)).thenReturn(true);
+        when(controller.initPlayer(player2)).thenReturn(true);
+
+        assertTrue(gameEngine.initPlayers());
+        verify(controller, times(1)).initPlayer(player1);
+        verify(controller, times(1)).initPlayer(player2);
     }
 
 
