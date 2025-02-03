0e75e6f5bc7f82ce9e2e5402a263275e8ee13cd2
Rayan Outili <rayanout2005@gmail.com>
2025-02-03 12:50:13 +0100

    feature: add verbose mode #96

--- src/fr/cotedazur/univ/polytech/ttr/equipeb/Main.java
+++ src/fr/cotedazur/univ/polytech/ttr/equipeb/Main.java
@@ -1,5 +1,7 @@
 package fr.cotedazur.univ.polytech.ttr.equipeb;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
 import fr.cotedazur.univ.polytech.ttr.equipeb.engine.GameEngine;
 import fr.cotedazur.univ.polytech.ttr.equipeb.exceptions.JsonParseException;
 import fr.cotedazur.univ.polytech.ttr.equipeb.factories.DestinationCardsFactory;
@@ -7,20 +9,41 @@ import fr.cotedazur.univ.polytech.ttr.equipeb.factories.MapFactory;
 import fr.cotedazur.univ.polytech.ttr.equipeb.factories.PlayerFactory;
 import fr.cotedazur.univ.polytech.ttr.equipeb.factories.WagonCardsFactory;
 import fr.cotedazur.univ.polytech.ttr.equipeb.models.deck.DestinationCardDeck;
-import fr.cotedazur.univ.polytech.ttr.equipeb.models.game.GameModel;
 import fr.cotedazur.univ.polytech.ttr.equipeb.models.deck.WagonCardDeck;
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.game.GameModel;
 import fr.cotedazur.univ.polytech.ttr.equipeb.models.map.Route;
 import fr.cotedazur.univ.polytech.ttr.equipeb.players.Player;
 import fr.cotedazur.univ.polytech.ttr.equipeb.players.models.PlayerIdentification;
 import fr.cotedazur.univ.polytech.ttr.equipeb.players.models.PlayerModel;
 import fr.cotedazur.univ.polytech.ttr.equipeb.players.models.PlayerType;
 import fr.cotedazur.univ.polytech.ttr.equipeb.players.views.PlayerConsoleView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        int verboseLevel = 2;
+
+        //on lit le paramètre --verbose=X si présent
+        for (String arg : args) {
+            if (arg.startsWith("--verbose=")) {
+                try {
+                    verboseLevel = Integer.parseInt(arg.split("=")[1]);
+                } catch (NumberFormatException e) {
+                    logger.info("Niveau de verbosité invalide, utilisation du niveau par défaut (INFO)");
+                }
+            }
+        }
+
+        setLogLevel(verboseLevel);
+
         try {
+            logger.debug("Démarrage de l'application...");
+
             List<Route> routes = (new MapFactory()).getMapFromJson();
             WagonCardDeck wagonCardDeck = new WagonCardDeck((new WagonCardsFactory()).getWagonCards());
             DestinationCardDeck destinationCardDeck = new DestinationCardDeck((new DestinationCardsFactory()).getShortDestinationCards());
@@ -41,8 +64,24 @@ public class Main {
             gameEngine.initPlayers();
             gameEngine.startGame();
 
+            logger.debug("Fin du jeu");
+
         } catch (JsonParseException e) {
-            e.printStackTrace();
+            logger.error("Erreur lors de la lecture du JSON: {}", e.getMessage());
+        }
+    }
+
+    /**
+     * Change dynamiquement le niveau de log.
+     * @param verboseLevel Niveau de verbosité : 1=ERROR/WARN, 2=INFO, 3=DEBUG.
+     */
+    public static void setLogLevel(int verboseLevel) {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        switch (verboseLevel) {
+            case 1 -> loggerContext.getLogger("fr.cotedazur.univ.polytech.ttr.equipeb").setLevel(Level.WARN);
+            case 2 -> loggerContext.getLogger("fr.cotedazur.univ.polytech.ttr.equipeb").setLevel(Level.INFO);
+            case 3 -> loggerContext.getLogger("fr.cotedazur.univ.polytech.ttr.equipeb").setLevel(Level.DEBUG);
+            default -> loggerContext.getLogger("fr.cotedazur.univ.polytech.ttr.equipeb").setLevel(Level.INFO);
         }
     }
 }
