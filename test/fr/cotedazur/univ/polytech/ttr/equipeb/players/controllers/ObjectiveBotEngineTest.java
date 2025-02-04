936a8ff802085fe6c80f1d5ff6c6567c3f6fcaa5
Maksimenka Matvei <70953270+MatveiMKS@users.noreply.github.com>
2025-02-04 09:58:01 +0100

    refactor: #97 add new ObjectiveBot

---
+++ test/fr/cotedazur/univ/polytech/ttr/equipeb/players/controllers/ObjectiveBotEngineTest.java
@@ -0,0 +1,32 @@
+package fr.cotedazur.univ.polytech.ttr.equipeb.players.controllers;
+
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.cards.DestinationCard;
+import fr.cotedazur.univ.polytech.ttr.equipeb.models.map.City;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ObjectiveBotEngineTest {
+
+    /*
+    @Test
+    void findDestinationsWithMostCommonCities() {
+        City city1 = new City("city1");
+        City city2 = new City("city2");
+        City city3 = new City("city3");
+        City city4 = new City("city4");
+        City city = new City("city");
+
+        DestinationCard card1 = new DestinationCard(city1, city2, 10);
+        DestinationCard card2 = new DestinationCard(city, city3, 10);
+
+        List<DestinationCard> cards = List.of(card1, card2);
+        ObjectiveBotEngine engine = new ObjectiveBotEngine(null, null, null);
+        List<DestinationCard> pair = engine.findDestinationsWithMostCommonCities(cards);
+        System.out.println(pair.get(0).getCities());
+        System.out.println(pair.get(1).getCities());
+    }
+    */
+}
